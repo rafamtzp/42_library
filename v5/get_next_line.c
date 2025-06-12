709ab89a01f69b0226b3cbc440b878f245cf1fa0
rafamtzp <rafamtz2001@gmail.com>
2025-06-12 18:43:55 +0200

    todo perfecto

--- v5/get_next_line.c
+++ v5/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ramarti2 <ramarti2@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:22:44 by ramarti2          #+#    #+#             */
-/*   Updated: 2025/06/12 17:55:50 by ramarti2         ###   ########.fr       */
+/*   Updated: 2025/06/12 18:42:05 by ramarti2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ static char	*appendbufs(int fd, char *buffer, char *leftovers)
 	{
 		bytesread = read(fd, buffer, BUFFER_SIZE);
 		if (bytesread < 0)
-			return (freebuf(&leftovers));
+			return (NULL);
 		else if (bytesread == 0)
 			break ;
 		if (!leftovers)
@@ -77,7 +77,7 @@ char	*get_next_line(int fd)
 	static char	*leftovers;
 
 	linebuf = NULL;
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, NULL, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (freebuf(&leftovers));
 	buffer = ft_calloc(1, BUFFER_SIZE + 1);
 	if (!buffer)
@@ -85,7 +85,7 @@ char	*get_next_line(int fd)
 	linebuf = appendbufs(fd, buffer, leftovers);
 	freebuf(&buffer);
 	if (!linebuf)
-		return (NULL);
+		return (freebuf(&leftovers));
 	leftovers = setline(linebuf);
 	if (!leftovers)
 		freebuf(&linebuf);
@@ -94,49 +94,8 @@ char	*get_next_line(int fd)
 	return (linebuf);
 }
 
-/*#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-int next_read_error = 0;
-
-int main(void)
-{
-    int fd = open("read_error.txt", O_RDONLY);
-    char *line;
-
-    // Lecturas normales
-    line = get_next_line(fd); printf("L1: %s", line); free(line);
-    line = get_next_line(fd); printf("L2: %s", line); free(line);
-
-    // Simular fallo de read()
-    next_read_error = 1;
-	close(fd);
-    line = get_next_line(fd);
-    if (line == NULL)
-        printf("L3: read() failed correctly.\n");
-    else
-    {
-        printf("ERROR: debería devolver NULL\n");
-        free(line);
-    }
-    next_read_error = 0;
-    close(fd);
-
-    // Reabrimos el archivo (para reproducir el comportamiento de los tests)
-    fd = open("read_error.txt", O_RDONLY);
-    while ((line = get_next_line(fd)) != NULL)
-    {
-        printf("Line: %s", line);
-        free(line);
-    }
-    close(fd);
-
-    return 0;
-}*/
-
-
+/*
+MAIN BUENO:
 #include <fcntl.h>
 #include <stdio.h>
 int	main(void)
@@ -156,10 +115,8 @@ int	main(void)
 	linebuf = get_next_line(fd);
 	printf("linea 3: %s", linebuf);
 	//free(linebuf);
-
-	
-	
-}	
+}
+*/	
 
 /*#include <fcntl.h>
 #include <stdio.h>
